5e10176183613ab91812ee3cd4718d8ce1360dd3
Akadet <akadet85@gmail.com>
2016-10-12 18:53:18 +0700

    day 8 - initial

---
+++ day8/proj_web_firebase/src/components/course-create.component.js
@@ -0,0 +1,27 @@
+class CourseCreateController {
+    constructor ($course, $state) {
+        'ngInject'
+
+        this.name = ''
+        this.description = ''
+
+        this.$course = $course
+        this.$state = $state
+    }
+
+    submit() {
+        this.$course.create({
+            name: this.name,
+            description: this.description
+        })
+            .subscribe((res) => {
+                this.$state.go('course-view', { id: res.key })
+            })
+    }
+}
+
+export default {
+    selector: 'courseCreate',
+    template: require('./course-create.component.html'),
+    controller: CourseCreateController
+}
---
+++ day8/proj_web_firebase/src/services/me.service.js
@@ -0,0 +1,24 @@
+export class MeService {
+    constructor($firebase) {
+        'ngInject'
+
+        this.$firebase = $firebase
+    }
+
+    saveProfile(profile) {
+        return this.$firebase.currentUser()
+            .flatMap((currentUser) => this.$firebase.set(`user/${currentUser.uid}`, profile))
+    }
+
+    getProfile(callback) {
+        return this.$firebase.currentUser()
+            .filter((currentUser) => currentUser !== undefined)
+            .flatMap((currentUser) => this.$firebase.onValue(`user/${currentUser.uid}`))
+    }
+
+    upload(file) {
+        return this.$firebase.currentUser()
+            .flatMap(({ uid }) => this.$firebase.upload(`user/${uid}/${Date.now()}`, file))
+            .map((res) => res.downloadURL)
+    }
+}
